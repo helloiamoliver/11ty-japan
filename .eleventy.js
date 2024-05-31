3a4ff77d81c853806e60d405b314b58da9ae836b
Oliver Hewitt <66276379+helloiamoliver@users.noreply.github.com>
2024-05-31 12:22:57 +0100

    Added passthrough copy?

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ const markdownIt = require("markdown-it");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("media");
+    eleventyConfig.addPassthroughCopy("admin/config.yml");
 
     eleventyConfig.addFilter("formatDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toISODate();
